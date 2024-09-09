97c45ebefc5ae03a5785b174d9b1ed7efbbb3efb
Song.ch_ <64467480+Song42@users.noreply.github.com>
2024-09-09 19:22:47 +0200

    Add colors support to your io interface. (#13)
    * Color Control: SGR (Select Graphic Rendition)
    * currently supports only 0-15 colors (format: ESC[;m)
    * added atoi function for string manipulation
    * added substring function for string manipulation
    * separated print/println functions into another file
    
    * Refactoring function syntax_check

---
+++ src/include/string.rs
@@ -0,0 +1,22 @@
+pub fn substring_between(s: &str, start_char: char, end_char: char) -> Option<&str> {
+	let start_pos = s.find(start_char)?;
+	let end_pos = s[start_pos + 1..].find(end_char)? + start_pos + 1;
+	Some(&s[start_pos + 1..end_pos])
+}
+
+pub fn atoi(s: &str) -> Result<usize, &'static str> {
+	let mut result: usize = 0;
+	let chars = s.chars();
+
+	if chars.clone().next() == Some('-') {
+		return Err("Negative numbers are not supported for usize");
+	}
+	for ch in chars {
+		if !ch.is_ascii_digit() {
+			return Err("Invalid character");
+		}
+		let digit = (ch as u8 - b'0') as usize;
+		result = result * 10 + digit;
+	}
+	Ok(result)
+}
