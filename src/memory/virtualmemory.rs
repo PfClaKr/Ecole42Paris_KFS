b3bd3187b0e6ad526451b0ace5809b297e1bce1f
Song42 <logoshope.song@gmail.com>
2024-10-12 02:33:34 +0200

    fix get_page_table with recursive paging

--- src/memory/virtualmemory.rs
+++ src/memory/virtualmemory.rs
@@ -30,16 +30,16 @@ pub enum PageFault {
 
 // First 3GB
 // #[allow(unused)]
-// const USER_SPACE_LOW: u32 = 0x00000000;
-// const USER_SPACE_HIGH: u32 = 0xBFFFFFFF;
+// const USER_SPACE_LOW: usize = 0x00000000;
+// const USER_SPACE_HIGH: usize = 0xBFFFFFFF;
 
 // Next 1GB
 // #[allow(unused)]
-// const KERNEL_SPACE_LOW: u32 = 0xC0000000;
-// const KERNEL_SPACE_HIGH: u32 = 0xFFFFFFFF;
+// const KERNEL_SPACE_LOW: usize = 0xC0000000;
+// const KERNEL_SPACE_HIGH: usize = 0xFFFFFFFF;
 
-type PageDirectoryEntry = u32;
-type PageTableEntry = u32;
+type PageDirectoryEntry = usize;
+type PageTableEntry = usize;
 
 #[allow(unused)]
 const ENTRY_COUNT: usize = 1024;
@@ -62,46 +62,41 @@ impl PageDirectory {
 		}
 	}
 	#[allow(unused)]
-	pub fn get_page_table(&mut self, pdi: u32) -> &mut PageTable {
-		let page_table_address = self.entry[pdi as usize] & 0xFFFFF000;
-		let page_table_ptr = page_table_address as *mut PageTable;
-
-		unsafe { &mut *page_table_ptr }
+	pub fn get_page_table(&mut self, address: usize) -> &mut PageTable {
+		let ptr = (0xFFC00000 + (address * 0x1000)) as *mut PageTable;
+		unsafe { &mut *ptr }
 	}
 	#[allow(unused)]
 	pub fn map_page(
 		&mut self,
-		virtual_address: u32,
-		physical_address: u32,
+		virtual_address: usize,
+		physical_address: usize,
 	) -> Result<(), PhysicalMemoryError> {
-		// physical address must be set on bitmap before entering in this function
 		let pdi = (virtual_address >> 22) & 0x3FF;
 		let pti = (virtual_address >> 12) & 0x3FF;
+		let page_table = self.get_page_table(pti);
 
-		// let page_table = 0xFFC00000 + (index * 0x1000);
-
-		if self.entry[pdi as usize] == 0 {
-			self.entry[pdi as usize] = BITMAP.lock().alloc_frame().unwrap() as u32;
+		if PAGE_DIRECTORY.lock().entry[pdi as usize] == 0 {
+			PAGE_DIRECTORY.lock().entry[pdi as usize] = BITMAP.lock().alloc_frame().unwrap() as usize;
 		}
-		let page_table = self.get_page_table(pdi);
-		page_table.entry[pti as usize] = (physical_address & 0xFFFFF000) | 0x3;
+		page_table.entry[pti] = (physical_address & 0xFFFFF000) | 0x3;
 		Ok(())
 	}
 	#[allow(unused)]
-	pub fn unmap_page(&mut self, virtual_address: u32) -> Result<(), PhysicalMemoryError> {
+	pub fn unmap_page(&mut self, virtual_address: usize) -> Result<(), PhysicalMemoryError> {
 		let pdi = (virtual_address >> 22) & 0x3FF;
 		let pti = (virtual_address >> 12) & 0x3FF;
 
 		assert_eq!(
-			self.entry[pdi as usize] & 0x1,
+			self.entry[pdi] & 0x1,
 			0,
 			"unmap_page: invalid address"
 		);
 		let page_table = self.get_page_table(pdi);
-		let mut page_table_entry = page_table.entry[pti as usize];
+		let mut page_table_entry = page_table.entry[pti];
 		if page_table_entry & 0x1 != 0 {
 			let frame = page_table_entry & 0xFFFFF000;
-			BITMAP.lock().free_frame(frame as usize)?;
+			BITMAP.lock().free_frame(frame)?;
 			page_table_entry = 0;
 			// Invalidate the page in the Translation Lookaside Buffer
 			unsafe {
@@ -111,22 +106,23 @@ impl PageDirectory {
 		Ok(())
 	}
 	#[allow(unused)]
-	pub fn translate(&self, virtual_address: u32, page_table: &PageTable) -> Option<u32> {
+	pub fn translate(&self, virtual_address: usize, page_table: &PageTable) -> Option<usize> {
 		let pdi = (virtual_address >> 22) & 0x3FF;
 		let pti = (virtual_address >> 12) & 0x3FF;
 		let offset = virtual_address & 0xFFF;
 
-		if self.entry[pdi as usize] & 0x1 == 0 {
+		if self.entry[pdi] & 0x1 == 0 {
 			return None;
 		}
-		if page_table.entry[pti as usize] & 0x1 == 0 {
+		if page_table.entry[pti] & 0x1 == 0 {
 			return None;
 		}
-		let frame = page_table.entry[pti as usize] & 0xFFFFF000; // 0b11111111_11111111_11110000_00000000
+		let frame = page_table.entry[pti] & 0xFFFFF000; // 0b11111111_11111111_11110000_00000000
 		Some(frame + offset)
 	}
 }
 
+#[allow(unused)]
 impl PageTable {
 	pub fn new() -> Self {
 		PageTable {
@@ -140,7 +136,7 @@ pub static PAGE_DIRECTORY: Mutex<PageDirectory> = Mutex::new(PageDirectory {
 });
 
 pub fn enable_recursive() {
-	let pd = &PAGE_DIRECTORY.lock() as *const _ as u32;
+	let pd = &PAGE_DIRECTORY as *const _ as usize;
 	PAGE_DIRECTORY.lock().entry[1023] = pd | 0x3;
 }
 
@@ -157,41 +153,48 @@ pub fn init() {
 	//
 	// Identity Mapped addresses are usually remapped after kernel load
 
-	println!("Page Directory Address Before Enabling Paging: {:x}", &PAGE_DIRECTORY.lock() as *const _ as u32);
 	enable_recursive();
-	
-	PAGE_DIRECTORY
-		.lock()
-		.map_page(0x0, 0x0)
-		.unwrap();
+
+	// let mut page_table = PageTable::new();
+	// for i in 0..ENTRY_COUNT {
+	// 	page_table.entry[i] = (i * 0x1000) as usize | 0x3;
+	// }
+	// PAGE_DIRECTORY.lock().entry[0] = (&page_table as *const _ as usize) | 0x3;
+
+	PAGE_DIRECTORY.lock().map_page(0x0, 0x0).unwrap();
+
+	// for i in 0..ENTRY_COUNT {
+	// 	PAGE_DIRECTORY
+	// 		.lock()
+	// 		.map_page(i, i)
+	// 		.unwrap();
+	// }
 
 	// Map User Space and Kernel Space pages
 	// 768 / 256
 
-	for entry in PAGE_DIRECTORY.lock().entry.iter_mut().enumerate() {
-		if entry.0 < 256 {
-			// kernel space
-		} else {
-			// user space
-		}
-	}
+	// for entry in PAGE_DIRECTORY.lock().entry.iter_mut().enumerate() {
+	// 	if entry.0 < 256 {
+	// 		// kernel space
+	// 	} else {
+	// 		// user space
+	// 	}
+	// }
 
 	// map VGA buffer (at 0xB8000) usually the size of the VGA_BUFFER is 4KB
 	// but can be up to 8KB depending on the display mode
 
 	// let vga_index = VGA_BUFFER_ADDRESS / 0x1000;
-	// page_table.entry[vga_index] = VGA_BUFFER_ADDRESS as u32 | 0x1 | 0x01;
+	// page_table.entry[vga_index] = VGA_BUFFER_ADDRESS | 0x1 | 0x01;
 
 	unsafe {
 		load_page_directory();
 		enable_paging();
 	}
-
-	println!("Page Directory Address After Enabling Paging: {:x}", &PAGE_DIRECTORY.lock() as *const _ as u32);
 }
 
 unsafe fn load_page_directory() {
-	let page_directory_address = &PAGE_DIRECTORY.lock() as *const _ as u32;
+	let page_directory_address = &PAGE_DIRECTORY as *const _ as usize;
 	asm!(
 		"mov cr3, {0:e}",
 		in(reg) page_directory_address,
@@ -200,7 +203,7 @@ unsafe fn load_page_directory() {
 }
 
 unsafe fn enable_paging() {
-	let mut cr0: u32;
+	let mut cr0: usize;
 	asm!("mov {0:e}, cr0", out(reg) cr0);
 	cr0 |= 0x80000000; // 0b10000000_00000000_00000000_00000000
 	asm!("mov cr0, {0:e}", in(reg) cr0);
